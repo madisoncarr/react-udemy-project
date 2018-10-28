ce2fbb7da6df5e50d588972366490a7c9d84b5f9
madisoncarr <madison.carr@utexas.edu>
2018-10-28 14:05:46 -0500

    video list rendering correctly

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -12,7 +12,7 @@ const VideoList = props => {
     );
   });
 
-  return <ul className="col-nd-4 list-group">{videoItems}</ul>;
+  return <ul className="list-group col-md-4">{videoItems}</ul>;
 };
 
 export default VideoList;
